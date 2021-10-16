b62e7cba4f06f541c7fe0a16d53f9c17982b2886
iqbalululazmi <iqbalululazmi@gmail.com>
2021-10-16 22:59:07 +0700

    feat: custom tailwind primary color

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,7 +1,25 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require('tailwindcss/colors')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      ...colors,
+      primary: {
+        50: '#f3f8f9',
+        100: '#daf1fa',
+        200: '#afe0f5',
+        300: '#7cc2e7',
+        400: '#479ed3',
+        500: '#357dc0',
+        600: '#2d62a9',
+        700: '#254a87',
+        800: '#1b3260',
+        900: '#101f3f',
+      },
+    },
     extend: {},
   },
   variants: {
