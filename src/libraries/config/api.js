1a3bcc1fa276aae610852172a6a42a209783f898
iqbalululazmi <iqbalululazmi@gmail.com>
2021-10-16 22:34:11 +0700

    feat: add api config

---
+++ src/libraries/config/api.js
@@ -0,0 +1,6 @@
+export const CONFIG = {
+  KEY: 'd212dc1bfc2d8009f736f68f2e71938f',
+  BASE_URL: 'https://api.themoviedb.org/3/',
+  BASE_IMAGE_URL: 'https://image.tmdb.org/t/p/w500/',
+  DEFAULT_LANGUAGE: 'en-us',
+}
