4726a1880fee527c4f900ac01ad5dd6bd1dc1e28
iqbalululazmi <iqbalululazmi@gmail.com>
2021-10-16 22:59:46 +0700

    feat: create navbar component

---
+++ src/components/Navbar/libraries/navbar.tsx
@@ -0,0 +1,101 @@
+import { Disclosure } from '@headlessui/react'
+import { MenuIcon, SearchIcon, XIcon } from '@heroicons/react/outline'
+import classNames from 'classnames'
+import Image from 'next/image'
+
+const navigation = [
+  { name: 'Movies', href: '/movie', current: false },
+  { name: 'TV Shows', href: '/tv-show', current: false },
+  { name: 'Latest', href: '/latest', current: false },
+  { name: 'My List', href: '/my-list', current: false },
+]
+
+const NavbarComponent = () => {
+  return (
+    <Disclosure as="nav" className="bg-primary-900">
+      {({ open }) => (
+        <>
+          <div className="max-w-full mx-auto px-2 sm:px-6 lg:px-16">
+            <div className="relative flex items-center justify-between h-16">
+              <div className="absolute inset-y-0 left-0 flex items-center sm:hidden">
+                {/* Mobile menu button*/}
+                <Disclosure.Button className="inline-flex items-center justify-center p-2 rounded-md text-gray-400 hover:text-white hover:bg-gray-700 focus:outline-none focus:ring-2 focus:ring-inset focus:ring-white">
+                  <span className="sr-only">Open main menu</span>
+                  {open ? (
+                    <XIcon className="block h-6 w-6" aria-hidden="true" />
+                  ) : (
+                    <MenuIcon className="block h-6 w-6" aria-hidden="true" />
+                  )}
+                </Disclosure.Button>
+              </div>
+              <div className="flex-1 flex items-center justify-center sm:items-stretch sm:justify-start">
+                <div className="flex-shrink-0 flex items-center">
+                  <Image
+                    className="hidden lg:block h-8 w-auto"
+                    src="/tmdb.svg"
+                    alt="Workflow"
+                    width={50}
+                    height={50}
+                  />
+                </div>
+                <div className="hidden sm:ml-6 sm:flex sm:flex-row sm:items-center">
+                  <div className="flex space-x-4">
+                    {navigation.map((item) => (
+                      <a
+                        key={item.name}
+                        href={item.href}
+                        className={classNames(
+                          item.current
+                            ? 'bg-gray-900 text-white'
+                            : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                          'px-3 py-2 rounded-md text-sm font-medium'
+                        )}
+                        aria-current={item.current ? 'page' : undefined}
+                      >
+                        {item.name}
+                      </a>
+                    ))}
+                  </div>
+                </div>
+              </div>
+              <div className="absolute inset-y-0 right-0 flex gap-3 items-center pr-2 sm:static sm:inset-auto sm:ml-6 sm:pr-0">
+                <div className="text-gray-400 hover:text-white hidden sm:block">
+                  <a href="/login">Login</a>
+                </div>
+                <button
+                  type="button"
+                  className="bg-gray-800 p-1 rounded-full text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-offset-gray-800 focus:ring-white"
+                >
+                  <span className="sr-only">View notifications</span>
+                  <SearchIcon className="h-6 w-6" aria-hidden="true" />
+                </button>
+              </div>
+            </div>
+          </div>
+
+          <Disclosure.Panel className="sm:hidden">
+            <div className="px-2 pt-2 pb-3 space-y-1">
+              {navigation.map((item) => (
+                <a
+                  key={item.name}
+                  href={item.href}
+                  className={classNames(
+                    item.current
+                      ? 'bg-gray-900 text-white'
+                      : 'text-gray-300 hover:bg-gray-700 hover:text-white',
+                    'block px-3 py-2 rounded-md text-base font-medium'
+                  )}
+                  aria-current={item.current ? 'page' : undefined}
+                >
+                  {item.name}
+                </a>
+              ))}
+            </div>
+          </Disclosure.Panel>
+        </>
+      )}
+    </Disclosure>
+  )
+}
+
+export default NavbarComponent
