03b8b1c5ecbb0fec88e4b2c90687760b56657939
iqbalululazmi <iqbalululazmi@gmail.com>
2021-10-17 19:20:10 +0700

    feat: create disclosure component

---
+++ src/components/Disclosure/libraries/disclosure.tsx
@@ -0,0 +1,28 @@
+import { Disclosure } from '@headlessui/react'
+import { ChevronUpIcon } from '@heroicons/react/solid'
+import { IDisclosureProps } from './disclosure.type'
+
+const DisclosureComponent = ({ title, children }: IDisclosureProps) => {
+  return (
+    <div className="w-full">
+      <div className="w-full max-w-md p-2 mx-auto bg-white rounded-2xl border">
+        <Disclosure>
+          {({ open }) => (
+            <>
+              <Disclosure.Button className="flex justify-between w-full px-4 py-2 text-sm font-medium text-left rounded-lg bg-gray-100 hover:bg-gray-200 focus:outline-none focus-visible:ring focus-visible:ring-purple-500 focus-visible:ring-opacity-75">
+                <span>{title}</span>
+                <ChevronUpIcon
+                  className={`${open ? 'transform rotate-180' : ''} w-5 h-5`}
+                />
+              </Disclosure.Button>
+              <Disclosure.Panel className="px-4 pt-4 pb-2 text-sm">
+                {children}
+              </Disclosure.Panel>
+            </>
+          )}
+        </Disclosure>
+      </div>
+    </div>
+  )
+}
+export default DisclosureComponent
---
+++ src/components/Disclosure/libraries/disclosure.type.ts
@@ -0,0 +1,6 @@
+import { ReactNode } from 'react'
+
+export type IDisclosureProps = {
+  title: string
+  children: ReactNode
+}
