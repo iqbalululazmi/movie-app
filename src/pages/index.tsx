789ae614540a6a5f5b1ff3a2ed10e9708e861313
iqbalululazmi <iqbalululazmi@gmail.com>
2021-10-17 16:19:47 +0700

    feat: fetch trending and popular api on serverSideProps

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,26 @@
 import HomeContainer from '@containers/home/libraries/home'
-import type { NextPage } from 'next'
+import MoviesRepository from '@libraries/repositories/movies'
+import { IHomeResponseProps } from '@libraries/types/home.type'
 
-const Home: NextPage = () => {
+export async function getServerSideProps() {
+  const trendingData = await MoviesRepository().fetchTrendingMovie()
+  const popularData = await MoviesRepository().fetchPopularMovie()
+
+  if (!trendingData) {
+    return {
+      notFound: true,
+    }
+  }
+
+  return {
+    props: { trending: trendingData.results, popular: popularData.results },
+  }
+}
+
+const Home = (props: IHomeResponseProps) => {
   return (
     <>
-      <HomeContainer />
+      <HomeContainer {...props} />
     </>
   )
 }
